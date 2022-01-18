9c782561b59c53a86bb06612ec39f6da08d71fef
jo4356 <joyce_tzu_chia_wang@yahoo.ca>
2022-01-18 14:57:45 -0800

    fix test file import, add percent_mass test

--- tests/test_chemcalculator.py
+++ tests/test_chemcalculator.py
@@ -1,1 +1,22 @@
-from chemcalculator import chemcalculator
+from chemcalculator.chemcalculator import percent_mass
+
+
+
+
+
+def test_percent_mass():
+    """
+    Test the percent_mass function for correct output
+    """
+    assert percent_mass("H2O", "O") == pytest.approx(88.79, rel=1e-3)
+    
+    assert percent_mass("H2O", "H") == pytest.approx(5.6048, rel=1e-3)
+
+    assert percent_mass("H2O", "H2") == pytest.approx(11.209, rel=1e-3)
+
+    assert percent_mass("NaOH", "OH") == pytest.approx(42.525, rel=1e-3)
+
+    assert percent_mass("H2SO4", "SO4") == pytest.approx(97.9404, rel=1e-3)
+
+    assert percent_mass("H2SO4", "HSO4") == pytest.approx(98.9702, rel=1e-3)
+    
