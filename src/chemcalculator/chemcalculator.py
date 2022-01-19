2e64bfdb80b08317ce1311aa9acedb3b0bae4875
datallurgy <allyson.m.stoll@gmail.com>
2022-01-18 16:20:28 -0800

    Created decomposition helper function for error checking

--- src/chemcalculator/chemcalculator.py
+++ src/chemcalculator/chemcalculator.py
@@ -28,7 +28,7 @@ def compute_mass(chemical):
 
     Raises
     ------
-    ValueError
+    TypeError
         Entered value is not a string.
     ValueError
         String contains characters that are not allowed.
@@ -45,91 +45,36 @@ def compute_mass(chemical):
 
     >>> compute_mass("Al2(SO4)3")
     342.15
+    
+    >>> compute_mass("(NH4)HS")
+    51.111
     """
     allowed_characters = r'[^\(\)A-Za-z0-9]'
     not_allowed_lowercase = r'^[a-z]|\([a-z]'
     
-    if isinstance(chemical, str): break
+    if isinstance(chemical, str): pass
     else:
-        raise ValueError:
-            print('Entered value is not a string')
+        raise TypeError('Entered value is not a string')
             
     if re.search(allowed_characters, chemical):
-        raise ValueError:
-            print('String contains characters that are not allowed.')
-    else: break
+        raise ValueError('String contains characters that are not allowed.')
+    else: pass
     
     if re.search(not_allowed_lowercase, chemical):
-        raise ValueError:
-            print('String or subcomponent starts with a lowercase letter.')
-    else: break
-    
-    primary_list = []
-    temp_primary_list = []
-    compound_list = []
-    simplified_compounds_list = []
-    raw_element_list = []
-    
-    def decompose_elements(string):
-        temp_list = re.findall(r'(\(.*?\)\d+)|([A-Z][^A-Z|(]*)', string)
-        temp_list = [item for sublist in temp_list for item in sublist]
-        temp_list = list(filter(None, temp_list))
-        return temp_list
-    
-    # split major components of the given chemical
-    primary_list = decompose_elements(chemical)
-    
-    # separate compounds from simple elements
-    for component in primary_list:
-        if re.match('\(.*?\)\d+', component):
-            compound_list.append(element)
-            primary_list.remove(element)
-            
-    # simplify the compounds
-    for compound in compound_list:
-        trim = re.findall('\)\d+', compound)
-        length = len(trim[0])
-        units = trim[0][1:]
-        
-        simplified_compound = compound[1:]
-        simplified_compound = simplified_compound[:-length]
-        
-        for i in range(int(units)):
-            simplified_compounds_list.append(simplified_compound)
-    
-    # decompose compounds
-    for compound in simplified_compounds_list:
-        temp_list = decompose_elements(compound)
-        temp_primary_list = temp_primary_list + temp_list
-    
-    # merge inital list with decomposed compounds
-    primary_list = primary_list + temp_primary_list
-    
-    # break down multiple atoms (e.g. Al2 = Al + Al)
-    for element in primary_list:
-        trim = re.findall('\d+', element)
-        if trim:
-            length = len(trim[0])
-            units = trim[0]
-            simplified_element = element[:-length]
-            
-        else:
-            length = 0
-            units = 1
-            simplified_element = element
-    
-        for i in range(int(units)):
-            raw_element_list.append(simplified_element) 
+        raise ValueError('String or subcomponent starts with a lowercase letter.')
+    else: pass
     
     # count and sum up all individual elements
-    elements = pd.DataFrame.from_dict(Counter(raw_element_list), orient='index', columns = ["Counts"]).reset_index()
-    elements = elements.rename(columns={"index": "Element"})
+    df = pd.DataFrame.from_dict(__chemical_elements(chemical),
+                                orient='index',
+                                columns = ["Counts"]).reset_index()
+    df = df.rename(columns={"index": "Element"})
     
     # map atomic masses from periodic table and multiply by atomic counts
-    elements['Mass'] = elements['Element'].map(periodic_table_mass) * elements['Counts']
+    df['Mass'] = df['Element'].map(periodic_table_mass) * df['Counts']
     
     # sum and return mass
-    return elements['Mass'].sum()
+    return df['Mass'].sum()
 
 def moles_grams_converter(formula, mass, convert_to):
     """
@@ -198,3 +143,83 @@ def percent_mass(compound, element):
     perc_mass = round(compute_mass(element)/compute_mass(compound)*100, 3)
     print(f"The percentage mass of {element} in {compound} is: {perc_mass} %")
     return perc_mass
+
+def __chemical_elements(chemical):
+    """Decomposes a chemical to it's elements.
+
+    Parameters
+    ----------
+    chemical : string
+        The molecular formula of the given chemical compound given as a string.
+
+    Returns
+    -------
+    dict
+        Dictionary of the chemicals elemental components and their counts.
+    """
+    primary_list = []
+    temp_primary_list = []
+    compound_list = []
+    simplified_compounds_list = []
+    raw_element_list = []
+    
+    def decompose_elements(string):
+        """Decompose string into list of components based on capital letters or parenteses."""
+        temp_list = re.findall(r'(\(.*?\)\d+)|([A-Z][^A-Z|(]*)', string)
+        temp_list = [item for sublist in temp_list for item in sublist]
+        temp_list = list(filter(None, temp_list))
+        return temp_list
+    
+    # split major components of the given chemical
+    primary_list = decompose_elements(chemical)
+    
+    # separate compounds from simple elements
+    for component in primary_list:
+        if re.match('\(.*?\)\d+', component):
+            compound_list.append(component)
+            primary_list.remove(component)
+            
+    # simplify the compounds
+    for compound in compound_list:
+        trim = re.findall('\)\d+', compound)
+        
+        if trim:
+            length = len(trim[0])
+            units = trim[0][1:]
+            simplified_compound = compound[1:]
+            simplified_compound = simplified_compound[:-length]
+            
+        else:
+            length = 1
+            units = 1
+            simplified_compound = compound[1:]
+            simplified_compound = simplified_compound[:-length]
+        
+        for i in range(int(units)):
+            simplified_compounds_list.append(simplified_compound)
+    
+    # decompose compounds
+    for compound in simplified_compounds_list:
+        temp_list = decompose_elements(compound)
+        temp_primary_list = temp_primary_list + temp_list
+    
+    # merge inital list with decomposed compounds
+    primary_list = primary_list + temp_primary_list
+    
+    # break down multiple atoms (e.g. Al2 = Al + Al)
+    for element in primary_list:
+        trim = re.findall('\d+', element)
+        if trim:
+            length = len(trim[0])
+            units = trim[0]
+            simplified_element = element[:-length]
+            
+        else:
+            length = 0
+            units = 1
+            simplified_element = element
+    
+        for i in range(int(units)):
+            raw_element_list.append(simplified_element) 
+    
+    return Counter(raw_element_list)
